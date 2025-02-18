e94368a91ce8ef38e8c0a87131cba92175423d91
“bharath <bharathwaj9600@gmail.com>
2025-02-18 12:55:27 +0530

    delete

--- webpack.mix.cjs
+++
@@ -1,7 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/scss/app.scss', 'public/css')
-   .copyDirectory('resources/img', 'public/img')
-   .setPublicPath('public');
-  
